c98b8a32fbba2e0ce425b300b4fd9f9c4c4dbca2
fluffybled <bledarkabashaj@gmail.com>
2020-12-06 14:31:52 +0100

    finished aoc_3_1

--- aoc_3_1/src/main.rs
+++ aoc_3_1/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let contents = std::fs::read_to_string("./res/input.txt").expect("Could not read file!");
+    let lines: Vec<&str> = contents.lines().collect();
+    let mut x_axis: usize = 0;
+    let mut y_axis: usize = 0;
+    let line_length = lines[0].len();
+    let mut count = 0;
+    loop {
+        x_axis += 3;
+        y_axis += 1;
+        if lines.get(y_axis).unwrap().chars().nth(x_axis % line_length).unwrap() == '#'{
+            count += 1;
+        }
+        if y_axis == lines.len() - 1 {
+            break;
+        }
+    }
+
+    println!("{}", count);
 }
