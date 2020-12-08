6d1a4f347982bc14eaf6b82cd387d95c87140c9b
fluffybled <bledarkabashaj@gmail.com>
2020-12-08 01:45:33 +0100

    aoc_4_1 solved

---
+++ aoc_4_1/src/main.rs
@@ -0,0 +1,27 @@
+
+
+fn main() {
+    let contents= std::fs::read_to_string("./res/input.txt").expect("Could not read file!");
+
+    let passports = contents.split("\n\n").collect::<Vec<&str>>();
+
+    let folded_passports = passports.iter().map(|&pass| {
+        let temp: String = pass.lines().collect::<Vec<&str>>().join(" ");
+        return temp;
+    });
+    let fields = ["byr","iyr","eyr","hgt","hcl","ecl","pid"];
+
+    let count = folded_passports.filter(|pass| {
+        for elem in fields.iter() {
+            if !pass.contains(elem){
+                return false;
+            }
+        }
+        return true;
+    }).collect::<Vec<String>>().len();
+
+    println!("{}", count);
+
+}
+
+// let _fields = ["byr","iyr","eyr","hgt","hcl","ecl","pid","cid"]
