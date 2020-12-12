177978f871363a2f7bf2c5376351f1de2e57cede
fluffybled <bledarkabashaj@gmail.com>
2020-12-12 02:45:17 +0100

    improved readability

--- aoc_2_1/src/main.rs
+++ aoc_2_1/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
     let contents = std::fs::read_to_string("./res/input.txt").expect("Could not read file");
-    let count: usize = contents.lines().collect::<Vec<&str>>().iter().map(|&l| { return l.split(['-',' ',':'].as_ref()).collect::<Vec<&str>>();}).filter(|elem| { return (elem.get(0).unwrap().parse::<usize>().unwrap()..=elem.get(1).unwrap().parse::<usize>().unwrap()).contains(&elem.get(4).unwrap().chars().filter(|&character| { return character == elem.get(2).unwrap().chars().nth(0).unwrap();}).count());}).count();
+    let count: usize = contents.lines().collect::<Vec<&str>>().iter().map(|&l| {
+            return l.split(['-',' ',':'].as_ref()).collect::<Vec<&str>>();
+        }).filter(|elem| {
+            return (elem.get(0).unwrap().parse::<usize>().unwrap()..=elem.get(1).unwrap().parse::<usize>().unwrap()).contains(&elem.get(4).unwrap().chars().filter(|&character| {
+                return character == elem.get(2).unwrap().chars().nth(0).unwrap();
+            }).count());})
+        .count();
     println!("{}", count);
 }
