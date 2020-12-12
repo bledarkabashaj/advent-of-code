9a4b75ca790c401a895acc8040d2abe0174bf914
fluffybled <bledarkabashaj@gmail.com>
2020-12-12 02:56:17 +0100

    optimized/shortened code

--- aoc_2_2/src/main.rs
+++ aoc_2_2/src/main.rs
@@ -1,22 +1,9 @@
 fn main() {
     let contents = std::fs::read_to_string("./res/input.txt").expect("Could not read file");
-    let workspace: Vec<(i32, i32, char, &str)> = contents.lines().collect::<Vec<&str>>().iter().map(|l| split_line(l) ).collect();
-    let result = workspace.iter().filter(|&elem| is_valid(elem)).count();
-    println!("{}", result);
-}
-
-
-
-pub fn split_line(line: &str) -> (i32, i32, char, &str){
-    let vec: Vec<&str> = line.split(" ").collect();
-    let minmax: Vec<i32> = vec[0].split("-").map(|l| l.parse::<i32>().unwrap()).collect();
-    let character = vec[1].chars().nth(0).unwrap();
-    let password = vec[2];
-    (minmax[0], minmax[1], character, password)
-}
-
-
-pub fn is_valid(element: &(i32, i32, char, &str)) -> bool {
-    let characters: Vec<char> = element.3.chars().collect();
-    (characters[(element.0 as usize) - 1] == element.2 && characters[(element.1 as usize) - 1] != element.2) || (characters[(element.0 as usize) - 1] != element.2 && characters[(element.1 as usize) - 1] == element.2)
+    let count: usize = contents.lines().collect::<Vec<&str>>().iter().map(|&l| {
+            return l.split(['-',' ',':'].as_ref()).collect::<Vec<&str>>();
+        }).filter(|elem| {
+            return (elem.get(4).unwrap().chars().nth(elem.get(0).unwrap().parse::<usize>().unwrap() - 1).unwrap() == elem.get(2).unwrap().chars().nth(0).unwrap()) ^ (elem.get(4).unwrap().chars().nth(elem.get(1).unwrap().parse::<usize>().unwrap() - 1).unwrap() == elem.get(2).unwrap().chars().nth(0).unwrap());
+        }).count();
+    println!("{}", count);
 }
