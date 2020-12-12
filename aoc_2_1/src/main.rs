cdb0f6de5e521c5d4cdf50b9d30dcae0912d44d6
fluffybled <bledarkabashaj@gmail.com>
2020-12-12 02:37:08 +0100

    condensed code to a single line for aoc_2_1

--- aoc_2_1/src/main.rs
+++ aoc_2_1/src/main.rs
@@ -1,22 +1,5 @@
 fn main() {
     let contents = std::fs::read_to_string("./res/input.txt").expect("Could not read file");
-    let count: usize = contents.lines().collect::<Vec<&str>>().iter().map(|&l| {split_line(l)} ).filter(|elem| is_valid(elem)).count();
+    let count: usize = contents.lines().collect::<Vec<&str>>().iter().map(|&l| { return l.split(['-',' ',':'].as_ref()).collect::<Vec<&str>>();}).filter(|elem| { return (elem.get(0).unwrap().parse::<usize>().unwrap()..=elem.get(1).unwrap().parse::<usize>().unwrap()).contains(&elem.get(4).unwrap().chars().filter(|&character| { return character == elem.get(2).unwrap().chars().nth(0).unwrap();}).count());}).count();
     println!("{}", count);
-}
-
-
-
-pub fn split_line(line: &str) -> (i32, i32, char, &str){
-    let minmax: Vec<&str> = line.split(['-',' ',':'].as_ref()).collect();
-    (minmax[0].parse::<i32>().unwrap(), minmax[1].parse::<i32>().unwrap(), minmax[2].chars().nth(0).unwrap(), minmax[4])
-}
-
-
-pub fn is_valid(element: &(i32, i32, char, &str)) -> bool {
-    let reps = element.3.chars().filter(|c| *c == element.2).count();
-    if reps >= element.0 as usize && reps <= element.1 as usize {
-        return true;
-    } else {
-        return false;
-    }
 }
